3cd7cfcda1fa5c7b1b985e2749fa94c41f4b4597
Churchill <churchillrimamskep@gmail.com>
2024-12-24 00:35:43 +0100

    able to retrive json data from the api

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\API\V1\CustomerController;
+use App\Http\Controllers\API\V1\InvoiceController;
+use App\Models\Customer;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::group(['prefix'=> 'v1','namespace'=>'App\Http\Controllers\API\V1'], function () {
+    Route::apiResource('customers',CustomerController::class);
+    Route::apiResource('invoice',InvoiceController::class);
 });
